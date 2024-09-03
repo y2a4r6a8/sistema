19c361a90a40a315e0488f42e84c56999ba090d5
y2a4r6a8 <108240893+y2a4r6a8@users.noreply.github.com>
2024-09-03 11:11:36 -0300

    codigo

--- views.py
+++ views.py
@@ -1,16 +1,16 @@
 from flask import render_template, request, redirect, url_for
 from app import app
-import sqlite3
+from models import get_db_connection
 
-def get_db_connection():
-    conn = sqlite3.connect('site.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
+# Rota para listar eventos
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    events = conn.execute('SELECT * FROM events').fetchall()
+    conn.close()
+    return render_template('index.html', events=events)
 
+# Rota para listar eventos
 @app.route('/events')
 def events():
     conn = get_db_connection()
@@ -18,6 +18,7 @@ def events():
     conn.close()
     return render_template('event_list.html', events=events)
 
+# Rota para criar um novo evento
 @app.route('/create_event', methods=('GET', 'POST'))
 def create_event():
     if request.method == 'POST':
@@ -29,4 +30,63 @@ def create_event():
         return redirect(url_for('events'))
     return render_template('create_event.html')
 
-# Adicione rotas semelhantes para participants, locations e roles
+# Rota para listar participantes
+@app.route('/participants')
+def participants():
+    conn = get_db_connection()
+    participants = conn.execute('SELECT * FROM participants').fetchall()
+    conn.close()
+    return render_template('participant_list.html', participants=participants)
+
+# Rota para criar um novo participante
+@app.route('/create_participant', methods=('GET', 'POST'))
+def create_participant():
+    if request.method == 'POST':
+        name = request.form['name']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO participants (name) VALUES (?)', (name,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('participants'))
+    return render_template('create_participant.html')
+
+# Rota para listar localizações
+@app.route('/locations')
+def locations():
+    conn = get_db_connection()
+    locations = conn.execute('SELECT * FROM locations').fetchall()
+    conn.close()
+    return render_template('location_list.html', locations=locations)
+
+# Rota para criar uma nova localização
+@app.route('/create_location', methods=('GET', 'POST'))
+def create_location():
+    if request.method == 'POST':
+        name = request.form['name']
+        address = request.form['address']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO locations (name, address) VALUES (?, ?)', (name, address))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('locations'))
+    return render_template('create_location.html')
+
+# Rota para listar papéis
+@app.route('/roles')
+def roles():
+    conn = get_db_connection()
+    roles = conn.execute('SELECT * FROM roles').fetchall()
+    conn.close()
+    return render_template('role_list.html', roles=roles)
+
+# Rota para criar um novo papel
+@app.route('/create_role', methods=('GET', 'POST'))
+def create_role():
+    if request.method == 'POST':
+        role_name = request.form['role_name']
+        conn = get_db_connection()
+        conn.execute('INSERT INTO roles (role_name) VALUES (?)', (role_name,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('roles'))
+    return render_template('create_role.html')
